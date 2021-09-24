f35910789473d6db9984cd9a0e2e19d973adfd15
David M <davidmusa1@hotmail.com>
2021-09-24 15:07:11 -0400

    Fixed syntax

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
         <body>
             <div id="inside_container">
                 <input type="text" id="input_dog" value="" placeholder="Write your dog breed">
-                <button id="request_joke" onclick="joke_request()">GET JOKES</button>
+                <button id="request_dog" onclick="dog_request()">GET BREED</button>
             </div>
         </body>
     </html>
