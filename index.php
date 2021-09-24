b8b9853c40fe50e7c2d2767177ea022172e39cf7
David M <davidmusa1@hotmail.com>
2021-09-24 15:05:04 -0400

    Initialized project added HTML file

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Dogs</title>
+        </head>
+        <body>
+            <div id="inside_container">
+                <input type="text" id="input_dog" value="" placeholder="Write your dog breed">
+                <button id="request_joke" onclick="joke_request()">GET JOKES</button>
+            </div>
+        </body>
+    </html>
