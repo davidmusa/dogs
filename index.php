fe9065494d83912bfbf8ae38b9eb562f72ee2b61
David M <davidmusa1@hotmail.com>
2021-09-24 16:53:03 -0400

    Erased to do it step by step

--- index.php
+++ index.php
@@ -11,5 +11,23 @@
                 <input type="text" id="input_dog" value="" placeholder="Write your dog breed">
                 <button id="request_dog" onclick="dog_request()">GET BREED</button>
             </div>
+            <div id="outside_container">Get the subbreed here</div>
+
+            <script>
+
+                function dog_request() {
+
+                var dogURL = "https://dog.ceo/api/breed";
+                var dog_input = document.getElementById("input_dog").value;
+                var endpoint = "https://dog.ceo/api/breed/"+dogURL+"/images/random";
+                var requested_endpoint = endpoint + dog_input;
+                
+                var output = "";
+
+                }
+            </script>                
+
         </body>
     </html>
+
+    
