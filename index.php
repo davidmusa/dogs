22dfda0674c1a9ad110ae14c43fbb2b181ac496b
David M <davidmusa1@hotmail.com>
2021-09-28 11:34:47 -0400

    Need dynamic parametres to look for breed by breed

--- index.php
+++ index.php
@@ -7,35 +7,61 @@
             <link rel="stylesheet" href="styles.css">
             <title>Dogs</title>
         </head>
+
         <body>
             
-            <div class="main_div">
-                <div class="header">
-                    <h1>Infinite Dog App</h1>
-                    <div id="breed">
-                        <input type="text" id="dog_breed_input">
-                        <button id="request_dog" onclick="dog_input()">GET BREED</button>
-                    </div>
-                </div>
-                <div class="slideshow" id="slideshow">
+            <div class="container">
 
-                </div>
+                <h1>Dogs</h1>
+
+                    <form>
+                        <label for="breed">Breed</label>
+                        <input type="search" name="phone" id="breed" placeholder="Enter Breed" title="dog breeds"/>
+                        <input type="submit" value="Get a dog pic!">
+                    </form>
+
+                        <section class="results hidden">
+                            <h2>Look at this dog!</h2>
+                            <img class="results-img" alt="placeholder">
+                        </section>
+
             </div>
 
-        <script>
-            
-            async function dog_input() {
-                
-                var word_input = document.getElementById("dog_breed_input").value;
-                var endpoint = "https://dog.ceo/api/breeds/list/all";
-                var requested_endpoint = endpoint + "?contains=" + word_input + "&amount=";
+            <script>
 
-                var output = "";
-
-                console.log(requested_endpoint)
-            }
+                'use strict';
                 
-        </script>                
+                function getDogImage(breed) {
+                    fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
+                    .then(response => response.json())
+                    .then(responseJson => 
+                    displayResults(responseJson))
+                    .catch(error => alert('Something went wrong. Try again later.'));
+                }
+
+                function displayResults(responseJson) {
+                    console.log(responseJson);
+                    //replace the existing image with the new one
+                    $('.results-img').replaceWith(
+                    `<img src="${responseJson.message}" class="results-img">`
+                    )
+                    //display the results section
+                    $('.results').removeClass('hidden');
+                }
+
+                function watchForm() {
+                    $('form').submit(event => {
+                    event.preventDefault();
+                    getDogImage($('#breed').val());
+                    });
+                }
+
+                $(document).ready(function() {
+                    console.log('App loaded! Waiting for submit!');
+                    watchForm();
+                });
+                    
+            </script>                
 
         </body>
     </html>
